c61bd5277845aafa1e9b412992e82656d5263016
Daniel <prnxchannel@gmail.com>
2019-08-06 20:42:46 +0200

    Added header

--- create-redirect.sh
+++ create-redirect.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-#             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#                     Version 2, December 2004
+# This file is licensed under the MIT License and is part of the "redirector" project.
+# Copyright (c) 2019 Daniel Riegler
 #
-#  Copyright (C) 2019 Daniel Riegler <prnxchannel@gmail.com>
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-#  Everyone is permitted to copy and distribute verbatim or modified
-#  copies of this license document, and changing it is allowed as long
-#  as the name is changed.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
-#             DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
-#    TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
-#
-#   0. You just DO WHAT THE FUCK YOU WANT TO.
-
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 # Helper script for the redirector project (https://github.com/seeleft/redirector)
-# This script can create redirects with a simple CLI
+# This script can create redirects via a simple CLI
+
+# Example:
+# chmod +x create-redirect.sh
+# ./create-redirect.sh \ 
+#       --LOCATION https://google.com \
+#       --KEY test \
+#       --WORKDIR /home/redirector
 
 # stoml toml parser settings
 # https://github.com/freshautomations/stoml
